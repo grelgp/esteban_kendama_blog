9cdee3edca1283e1d70b4524f56b0398e04269d8
Funcker <funcker39@gmail.com>
2023-10-11 18:53:52 +0200

    Added header and changed colors

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,22 @@ export default function Home() {
 
   return (
     <main>
+      <header>
+        <div className="bg-blue-50 py-2 px-4 flex flex-row justify-evenly items-center">
+          <a href="" className="text-xl">
+            Esteban Kendama
+          </a>
+          <a href="https://www.youtube.com/@EstebanKendama" className="flex items-center">
+            <img className="w-8 mx-4" src="https://www.svgrepo.com/show/13671/youtube.svg" alt="" />
+            <p className="text-xl">
+              Ma chaîne
+            </p>
+          </a>
+          <a href="" className="text-xl">
+            Informations
+          </a>
+        </div>
+      </header>
       <div className='mx-8 place-content-center grid columns-1 sm:grid-cols-auto-fit'>
         {names.map((name, index) => {
           return (
