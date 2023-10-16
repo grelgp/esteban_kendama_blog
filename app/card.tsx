ac9f549ddb20b01a468d586c3b39615a7bfece0c
Funcker <funcker39@gmail.com>
2023-10-16 10:45:00 +0200

    Using react-hover-video npm package

--- app/card.tsx
+++ app/card.tsx
@@ -1,20 +1,41 @@
 "use client"
 
+// @ts-ignore
+import HoverVideoPlayer from 'react-hover-video-player';
+
 export default function Card({ title, videoSrc }: { title: String, videoSrc: string }) {
+
+    let pauseAllVideos = () => {
+        console.log("Hover new video");
+        let length = document.getElementsByTagName("video").length;
+
+        for (let i = 0; i < length; i++) {
+            const video = document.getElementsByTagName("video").item(i);
+            if (!video) continue;
+            if (video.paused) continue;
+
+            video.pause();
+        }
+    }
+
     return (
-        <div className=' w-[90vw] sm:w-72 p-2 my-2 flex flex-col items-stretch mx-auto rounded-2xl border-white border-2 shadow-md'
-            onClick={() => console.log("test")}
-            onMouseEnter={event => { var element = (event.target as HTMLDivElement).getElementsByTagName("video")[0] as HTMLVideoElement; if (element) element.play() }}
+        <div className=' w-[90vw] sm:w-72 mt-2 flex flex-col items-stretch mx-auto rounded-2xl bg-white border-[1px] shadow-md'
+            onMouseEnter={event => { var element = (event.target as HTMLDivElement).getElementsByTagName("video")[0] as HTMLVideoElement; if (element) { pauseAllVideos(); element.play() } }}
             onMouseLeave={event => { var element = (event.target as HTMLDivElement).getElementsByTagName("video")[0] as HTMLVideoElement; if (element) element.pause() }}
         >
-            <p className='w-full text-center text-2xl'>{title}</p>
-            <div className='bg-white m-4 flex-1'>
-                <video
-                    className='videoCard m-auto'
+            <p className='w-full text-center text-xl my-2'>{title}</p>
+            <div className='rounded-b-2xl overflow-hidden'>
+                <HoverVideoPlayer
+                    videoSrc={videoSrc}
+                />
+
+                {/* <video
+                    className='videoCard m-auto rounded-b-2xl'
                     muted
+                    loop
                 >
                     <source src={videoSrc} type="video/mp4" />
-                </video>
+                </video> */}
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,23 @@ export default function Home() {
   }
 
   return (
-    <main>
+    <main className="bg-gray-100">
       <header>
-        <div className="bg-blue-50 py-2 px-4 flex flex-row justify-evenly items-center">
-          <a href="" className="text-xl">
+        <div className="bg-white border-b-2 py-2 px-4 sm:px-8 space-x-6 flex flex-row justify-between items-center">
+          <a href="" className="text-xl sm:text-2xl font-semibold">
             Esteban Kendama
           </a>
-          <a href="https://www.youtube.com/@EstebanKendama" className="flex items-center">
-            <img className="w-8 mx-4" src="https://www.svgrepo.com/show/13671/youtube.svg" alt="" />
-            <p className="text-xl">
-              Ma chaîne
-            </p>
-          </a>
-          <a href="" className="text-xl">
-            Informations
-          </a>
+          <div className="flex items-center space-x-6 sm:space-x-10">
+            <a href="" className="text-md sm:text-lg hover:underline underline-offset-4">
+              A propos
+            </a>
+            <a href="https://www.youtube.com/@EstebanKendama" className="flex items-center hover:underline underline-offset-4">
+              <img className="w-8 sm:mr-2" src="./youtube.svg" alt="" />
+              <p className="text-lg hidden sm:block ">
+                Ma chaîne
+              </p>
+            </a>
+          </div>
         </div>
       </header>
       <div className='mx-8 place-content-center grid columns-1 sm:grid-cols-auto-fit'>
