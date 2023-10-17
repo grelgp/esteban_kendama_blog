3a0446374faa3649f3376504d05edfe3dca4eac3
Funcker <funcker39@gmail.com>
2023-10-17 16:31:21 +0200

    Fixed white border bottom, improved aestetics

--- app/card.tsx
+++ app/card.tsx
@@ -19,24 +19,16 @@ export default function Card({ title, videoSrc }: { title: String, videoSrc: str
     }
 
     return (
-        <div className=' w-[90vw] sm:w-72 mt-2 flex flex-col items-stretch mx-auto rounded-2xl bg-white border-[1px] shadow-md'
+        <div className=' w-[90vw] sm:w-72 mt-2 flex flex-col items-stretch mx-auto rounded-2xl bg-white border-[1px] shadow-md pb-0 overflow-hidden'
             onMouseEnter={event => { var element = (event.target as HTMLDivElement).getElementsByTagName("video")[0] as HTMLVideoElement; if (element) { pauseAllVideos(); element.play() } }}
             onMouseLeave={event => { var element = (event.target as HTMLDivElement).getElementsByTagName("video")[0] as HTMLVideoElement; if (element) element.pause() }}
         >
-            <p className='w-full text-center text-xl my-2'>{title}</p>
-            <div className='rounded-b-2xl overflow-hidden'>
+            <p className='w-full text-center text-xl  border-b-[1px] py-2  border-gray-100'>{title}</p>
+            
                 <HoverVideoPlayer
                     videoSrc={videoSrc}
                 />
-
-                {/* <video
-                    className='videoCard m-auto rounded-b-2xl'
-                    muted
-                    loop
-                >
-                    <source src={videoSrc} type="video/mp4" />
-                </video> */}
-            </div>
+        
         </div>
     )
 }
