18e6b324697125b187635bb297cb016525de008b
BuildTools <funcker39@gmail.com>
2023-10-26 15:12:46 +0200

    added prisma and changed bg and added about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <body >{children}</body>
+      <body>
+        <header>
+          <div className="bg-white border-b-2 py-2 px-4 sm:px-8 space-x-6 flex flex-row justify-between items-center">
+            <a href="/#" className="text-xl sm:text-2xl font-semibold">
+              Esteban Kendama
+            </a>
+            <div className="flex items-center space-x-6 sm:space-x-10">
+              <a href="/about" className="text-md sm:text-lg hover:underline underline-offset-4">
+                A propos
+              </a>
+              <a href="https://www.youtube.com/@EstebanKendama" className="flex items-center hover:underline underline-offset-4">
+                <img className="w-8 sm:mr-2" src="./youtube.svg" alt="" />
+                <p className="text-lg hidden sm:block ">
+                  Ma chaîne
+                </p>
+              </a>
+            </div>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,25 +10,7 @@ export default function Home() {
   }
 
   return (
-    <main className="bg-gray-100">
-      <header>
-        <div className="bg-white border-b-2 py-2 px-4 sm:px-8 space-x-6 flex flex-row justify-between items-center">
-          <a href="" className="text-xl sm:text-2xl font-semibold">
-            Esteban Kendama
-          </a>
-          <div className="flex items-center space-x-6 sm:space-x-10">
-            <a href="" className="text-md sm:text-lg hover:underline underline-offset-4">
-              A propos
-            </a>
-            <a href="https://www.youtube.com/@EstebanKendama" className="flex items-center hover:underline underline-offset-4">
-              <img className="w-8 sm:mr-2" src="./youtube.svg" alt="" />
-              <p className="text-lg hidden sm:block ">
-                Ma chaîne
-              </p>
-            </a>
-          </div>
-        </div>
-      </header>
+    <main>
       <div className='mx-8 place-content-center grid columns-1 sm:grid-cols-auto-fit'>
         {names.map((name, index) => {
           return (
